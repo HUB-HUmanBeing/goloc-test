6b62d1f077655689dc5e56192490acd3b5ca1e64
J3m5 <anojems@gmail.com>
2017-07-03 16:53:43 +0200

    first user story finished

--- geoloc_add.js
+++ geoloc_add.js
@@ -3,22 +3,45 @@
  */
 $(function() {
 
-    $("#search").click(function () {
 
-        var address = $("#adress").val();
-        var url = "http://nominatim.openstreetmap.org/search/"+ address +"?format=json&addressdetails=1&limit=1";
-        $.getJSON(url, function (data) {
-            console.log(data);
-            var lat = data[0].lat ;
-            var lon = data[0].lon ;
-            var city = data[0].address.city ;
-            var country = data[0].address.country;
-            $('#lat').val(lat);
-            $('#lon').val(lon);
-            $('#city').val(city);
-            $('#country').val(country);
+  function initmap(lat, lon) {
 
-        });
-
+    var map = L.map('mapid').setView([lat, lon], 13);
+    // create the tile layer with correct attribution
+    var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {
+      minZoom: 8,
+      maxZoom: 12,
+      attribution: osmAttrib
     });
+
+    // start the map
+
+    L.marker([lat, lon]).addTo(map)
+      .bindPopup('Ales')
+      .openPopup();
+    map.addLayer(osm);
+
+  }
+
+  $("#search").click(function() {
+
+    var address = $("#adress").val();
+    var url = "http://nominatim.openstreetmap.org/search/" + address + "?format=json&addressdetails=1&limit=1";
+    $.getJSON(url, function(data) {
+      console.log(data);
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      var city = data[0].address.city;
+      var country = data[0].address.country;
+      $('#lat').val(lat);
+      $('#lon').val(lon);
+      $('#city').val(city);
+      $('#country').val(country);
+      initmap(lat, lon);
+    });
+
+  });
+
 });
