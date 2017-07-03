a701a57a9ff13733711ceb2cff1af321b50c8a13
J3m5 <anojems@gmail.com>
2017-07-03 14:47:48 +0200

    first commit

--- geoloc_add.js
+++ geoloc_add.js
@@ -1,3 +1,22 @@
 /**
  * Created by banquo on 03/07/17.
  */
+$(function() {
+
+  $("#search").click(function() {
+
+    var data = $("#adress").val();
+
+    // var formdata = (window.FormData) ? new FormData($form[0]) : null;
+    // var data = (formdata !== null) ? formdata : $form.serialize();
+    // alert(data);
+
+    var url = "http://nominatim.openstreetmap.org/?format=json&json_callback=cbaddressdetails=1&q=" + data + "&format=json&limit=1";
+    alert(url);
+    $.getJSON("http://nominatim.openstreetmap.org/?format=json&json_callback=cbaddressdetails=1&q=" + data + "&format=json&limit=1");
+  });
+
+  cb({
+
+  });
+});
