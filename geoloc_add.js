f3a2b68759d1849b31a6e229572684aa1a5e126d
J3m5 <anojems@gmail.com>
2017-07-03 16:30:45 +0200

    try

--- geoloc_add.js
+++ geoloc_add.js
@@ -1,8 +1,10 @@
 /**
  * Created by banquo on 03/07/17.
  */
-$(function() {
-
+// $(function() {
+//   $.ajaxSetup({
+// crossOrigin: true
+// });
   $("#search").click(function() {
 
     var data = $("#adress").val();
@@ -11,12 +13,26 @@ $(function() {
     // var data = (formdata !== null) ? formdata : $form.serialize();
     // alert(data);
 
-    var url = "http://nominatim.openstreetmap.org/?format=json&json_callback=cbaddressdetails=1&q=" + data + "&format=json&limit=1";
+    var url = "http://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=" + data + "&limit=1";
     alert(url);
-    $.getJSON("http://nominatim.openstreetmap.org/?format=json&json_callback=cbaddressdetails=1&q=" + data + "&format=json&limit=1");
-  });
+    // $.getJSON("http://nominatim.openstreetmap.org/?format=json&json_callback=cbaddressdetails=1&q=" + data + "&format=json&limit=1").done(function(cb){
 
-  cb({
+    $.ajax({
+         url:"http://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=" + data + "&limit=1",
+         dataType: 'json', // Notice! JSONP <-- P (lowercase)
+         success:function(json){
+             // do stuff with json (in this case an array)
+             console.log(json);
+             alert("Success");
+         },
+         error:function(){
+             alert("Error");
+         }
+    });
+// $.getJSON(url, null, function(data) {
+//   $( '.result' ).html(data);
+// });
+//     });
 
-  });
+
 });
