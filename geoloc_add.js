d11da881cb1d8ec5c55de416b5e585bc95cf3a01
robinBanquo <caramaschi@hotmail.com>
2017-07-03 16:27:45 +0200

    fist commit

--- geoloc_add.js
+++ geoloc_add.js
@@ -5,10 +5,19 @@ $(function() {
 
     $("#search").click(function () {
 
-        var adress = $("#adress").val();
-        var url = "http://nominatim.openstreetmap.org/search/"+ adress +"?format=json&addressdetails=1&limit=1&polygon_svg=1";
+        var address = $("#adress").val();
+        var url = "http://nominatim.openstreetmap.org/search/"+ address +"?format=json&addressdetails=1&limit=1";
         $.getJSON(url, function (data) {
-            console.log(data)
+            console.log(data);
+            var lat = data[0].lat ;
+            var lon = data[0].lon ;
+            var city = data[0].address.city ;
+            var country = data[0].address.country;
+            $('#lat').val(lat);
+            $('#lon').val(lon);
+            $('#city').val(city);
+            $('#country').val(country);
+
         });
 
     });
