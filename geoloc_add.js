0e0a79a2131ddb51f0dc6027b74c379d5e65b1b7
robinBanquo <caramaschi@hotmail.com>
2017-07-03 15:34:28 +0200

    fist commit

--- geoloc_add.js
+++ geoloc_add.js
@@ -3,20 +3,20 @@
  */
 $(function() {
 
-  $("#search").click(function() {
+    $("#search").click(function () {
 
-    var data = $("#adress").val();
+        var adress = $("#adress").val();
 
-    // var formdata = (window.FormData) ? new FormData($form[0]) : null;
-    // var data = (formdata !== null) ? formdata : $form.serialize();
-    // alert(data);
+        // var formdata = (window.FormData) ? new FormData($form[0]) : null;
+        // var data = (formdata !== null) ? formdata : $form.serialize();
+        // alert(data);
 
-    var url = "http://nominatim.openstreetmap.org/?format=json&json_callback=cbaddressdetails=1&q=" + data + "&format=json&limit=1";
-    alert(url);
-    $.getJSON("http://nominatim.openstreetmap.org/?format=json&json_callback=cbaddressdetails=1&q=" + data + "&format=json&limit=1");
-  });
+        //var url = "http://nominatim.openstreetmap.org/?format=json&json_callback=cbaddressdetails=1&q=" + data + "&format=json&limit=1&polygon_svg=1";
+        var url = "http://nominatim.openstreetmap.org/search/"+ adress +"?format=json&addressdetails=1&limit=1&polygon_svg=1";
+        $.getJSON(url, function (data) {
 
-  cb({
+        });
 
-  });
+    });
 });
+
